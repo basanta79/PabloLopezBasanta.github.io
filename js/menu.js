eec87e760200ed989de4aeb6bac1fe4d58638310
basanta79 <basanta79@gmail.com>
2018-12-21 11:58:38 +0100

    add animation

--- js/menu.js
+++ js/menu.js
@@ -1,23 +1,13 @@
 export function mngMenu(){
-
-    console.log('hola mundo')
+    
+    console.log('hola mundo ddd')
     let oMenuButton = document.querySelector('#btnMenu')
     let oMenuSelection = document.querySelector('#btnSelection')
-    let arrMenu = document.querySelectorAll('.mnu-btn')
-    console.log(arrMenu)
+    
     oMenuButton.addEventListener('click', toogleMenuMobile)
     oMenuSelection.addEventListener('click', toogleMenuMobile)
-    arrMenu.forEach(element =>{
-        element.addEventListener('click',scrollToSection)
-    })
+    
     /* arrMenu[1].addEventListener('click', scrollToSection) */
-
-    function scrollToSection(oEv){
-        console.log("scrolltosection:")
-        /* console.log(oEv) */
-        console.log(oEv.srcElement.id)
-        window.scroll(0,1000)
-    }
 
     function toogleMenuMobile(oEvent){
         /* console.log('has hecho click en boton') */
@@ -29,25 +19,77 @@ export function mngMenu(){
 }
 
 export function spyScroll(){
-    let aSections = document.querySelectorAll(".anchor")
-    let aMenu = document.querySelectorAll(".nav-left a")
+    let aSections = []
+    let aMenu = document.querySelectorAll(".nav-left li")
     let oSections = new Array
     let oSectionsSorted = new Array
-    sectionArrayBuilder()
+    let arrMenu = document.querySelectorAll('.mnu-btn')
+    /* console.log(arrMenu) */
+    /* sectionArrayBuilder() */
+    window.addEventListener('resize', calculateNewOffsets)
     window.addEventListener('scroll', changeMenu)
+    arrMenu.forEach(element =>{
+        element.addEventListener('click',scrollToSection)
+    })
+    sectionArrayBuilder();
+
+    function calculateNewOffsets(){
+        /* console.log("nuevos offsets") */
+        sectionArrayBuilder()
+    }
+
+    function scrollToSection(oEv){
+        
+        let desiredOffset =0
+        console.log("scrolltosection:")
+        /* console.log(oEv) */
+        console.log(oEv.srcElement.id)
+        /* window.scroll(0,1000) */
+        /* document.querySelector("#formulario").scrollIntoView({behaviour: 'smooth'}) */
+        /* document.scrollTop=0 */
+        let strNameTemp = oEv.srcElement.id.split('-')
+        console.log(strNameTemp[1])
+        oSectionsSorted.forEach(element => {
+            if (element.id == strNameTemp[1]){
+                desiredOffset = element.offset +20
+            }
+        })
+        
+        console.log("queremos ir a la section "+desiredOffset)
+        let index = window.pageYOffset;
+        console.log(index)
+        if (index<desiredOffset){
+            for (index =  0; index < desiredOffset; index++) {
+                setTimeout(function(){
+                    window.scrollTo(0, index)
+                },400)
+            }
+        }else{
+            for (index =  window.pageYOffset; index > desiredOffset; index--) {
+                setTimeout(function(){
+                    window.scrollTo(0, index)
+                },400)
+            }
+        }
+        
+        window.scroll(0, desiredOffset)
+        console.log(window.pageYOffset)
+    }
     
     function sectionArrayBuilder(){
-        oSections.push({"id":"Home", "offset": 0})
+        aSections = document.querySelectorAll(".anchor")
+        /* oSections.push({"id":"Home", "offset": 0}) */
+        oSections=[]
         aSections.forEach( element => {
-           /* console.log(element.id + element.offsetTop) */
+          /*  console.log(element.id + element.offsetTop) */
            oSections.push({"id":element.id, "offset": element.offsetTop})
         })
         oSectionsSorted = oSections.sort(function(a,b){
             return a['offset']-b['offset']
         })
         /* console.log(oSectionsSorted) */
-        aMenu.forEach( element =>{
-           /*  console.log(element.id) */
+        oSectionsSorted.forEach( element =>{
+            console.log(element.id + element.offset)
 
         })
     }
